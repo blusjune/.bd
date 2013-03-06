cc5346d170d5900d3174dec6cf4d9212bf303ea3
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-03-06 22:22:27 +0900

    exphist/sidewinder/

--- log/log.gpgizeme/exphist/sidewinder/sidewinder-20130306_171032/iowa/iowa.sim.iox/.bdx.0100.y.run_main_test.sh
+++ log/log.gpgizeme/exphist/sidewinder/sidewinder-20130306_171032/iowa/iowa.sim.iox/.bdx.0100.y.run_main_test.sh
@@ -2,18 +2,18 @@
 ##.bdx.0100.y.run_main_test.sh
 ##_ver=20130221_155606
 ##_ver=20130305_032931
-##_ver=20130306_170939
+##_ver=20130306_174929
 
 
-_cmd=".bdx.0110.n.run_sub_test.sh";
-
-_sio_percent=10;
+_sio_percent=25;
 _iow_size=100;
-_prd_t1=10000;
+_prd_t1=1000;
+_cmd=".bdx.0200.n.run_sub_test.sh";
 
 echo "$_sio_percent" > .ioc_threshold_percent;
 echo "$_iow_size" > .iow_size;
 echo "$_prd_t1" > .prd_t1;
+
 echo "SigIO_Threshold= ${_sio_percent} %; IOW_size= ${_iow_size} ; Periodicity_T1= ${_prd_t1}";
 
 . $_cmd;
--- log/log.gpgizeme/exphist/sidewinder/sidewinder-20130306_171032/iowa/iowa.sim.iox/.bdx.0200.n.run_sub_test.sh
+++ log/log.gpgizeme/exphist/sidewinder/sidewinder-20130306_171032/iowa/iowa.sim.iox/.bdx.0200.n.run_sub_test.sh
@@ -4,10 +4,12 @@
 
 
 _tstamp="$(tstamp)";
-_tdir="tdir/${_tstamp}";
 _cf_ioc_percent_file=".ioc_threshold_percent";
 _cf_iow_size_file=".iow_size";
 _cf_prd_t1=".prd_t1";
+
+
+
 
 if [ ! -f $_cf_ioc_percent_file ]; then
 	echo "#>> ERROR: '$_cf_ioc_percent_file' does not exist -- EXIT";
@@ -23,19 +25,26 @@ if [ ! -f $_cf_prd_t1 ]; then
 fi
 
 
+
+
+_ioc_percent="$(cat $_cf_ioc_percent_file)";
+_iow_size="$(cat $_cf_iow_size_file)";
+_prd_t1="$(cat $_cf_prd_t1)";
+_logfile="iowa.sigio_${_ioc_percent}.iowsz_${_iow_size}.t1_${_prd_t1}.log";
+
+_tdir="tdir/iowa.sim.iox.${_tstamp}.sigio_${_ioc_percent}.iowsz_${_iow_size}.t1_${_prd_t1}";
 if [ ! -d $_tdir ]; then
 	mkdir -p $_tdir;
 fi
 
 
-..ts
-_ioc_percent="$(cat $_cf_ioc_percent_file)";
-_iow_size="$(cat $_cf_iow_size_file)";
-_prd_t1="$(cat $_cf_prd_t1)";
-_logfile="iowa.sigio_${_ioc_percent}.iowsz_${_iow_size}.t1_${_prd_t1}.log";
-_infile=".tracelog.R.addr";
+
+
+_tstamp_begin=".job_begin__$(tstamp)"; touch $_tdir/$_tstamp_begin; echo "$_tstamp_begin";
 cat infile | bsc.iowa.lsp.sim.iox -c $_ioc_percent -w $_iow_size -p $_prd_t1 > $_tdir/$_logfile
-..ts
+_tstamp_finish=".job_finish__$(tstamp)"; touch $_tdir/$_tstamp_finish; echo "$_tstamp_finish";
+
+
 
 
 _mkfile_per_tag()
@@ -43,6 +52,8 @@ _mkfile_per_tag()
 	_fileout="__simout.sigio_${_ioc_percent}.iowsz_${_iow_size}.t1_${_prd_t1}.${_tag}.log";
 	cat $_tdir/$_logfile | grep $_tag > $_tdir/$_fileout;
 }
+
+
 
 
 _tag="__list__addr_hit_tstamp__"; _mkfile_per_tag;
