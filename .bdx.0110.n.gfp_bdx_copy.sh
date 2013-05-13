6329cfea98a3bdff8206b23403fd035305bf2510
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-05-14 00:37:57 +0900

    .blib/wiki/ update script added;

--- .bdx.0110.n.gfp_bdx_copy.sh
+++ .bdx.0110.n.gfp_bdx_copy.sh
@@ -7,14 +7,16 @@
 ##_ver=20130511_103052; #initial use of 'if [ "#$_value" = "#y" ]; then'
 
 
-_conf__b_enpack="n"; # default: "n"
-_conf__wikini_enpack="n"; # default: "n"
-_conf__wikini_enpack__wikini_dir="~/.blib/wiki/wikini";
+_conf_b__enpack="n"; # default: "n"
+_conf_wikini__enpack="y"; # default: "n"
+_conf_wikini__root="${HOME}/.blib/wiki/wikini";
+_conf_wikini__target_db="wikidb";
+_conf_wikini__mediawiki_var_root="/var/lib/mediawiki";
 
 
 
 
-if [ "#$_conf__b_enpack" = "#y" ]; then  # a chance to disable this operation
+if [ "#$_conf_b__enpack" = "#y" ]; then  # a chance to disable this operation
 #------------------------------------------------
 	echo "#>> update ~/.bd/.b (with ~/.b)"
 	~/.b/.bdx.d/.bdx.0200.y.clear_pyc.sh;
@@ -27,16 +29,38 @@ fi
 
 
 
-if [ "#$_conf__wikini_enpack" = "#y" ]; then
+if [ "#$_conf_wikini__enpack" = "#y" ]; then
 #------------------------------------------------
-	if [ ! -d $_conf__wikini_enpack__wikini_dir ]; then
-		mkdir -p $_conf__wikini_enpack__wikini_dir;
+	if [ ! -d $_conf_wikini__root ]; then
+		mkdir -p $_conf_wikini__root;
 	fi
+
 	(
-	cd $_conf__wikini_enpack__wikini_dir;
-	pwd;
-	echo ">> execute mysqldump for 'wikidb'";
-	echo ">> copy the uploaded wikini media data to the memory store";
+	cd $_conf_wikini__root;
+
+	_sql_dump_file="${_conf_wikini__target_db}.sql";
+	_var_lib_mediawiki_images_tarball="var_lib_mediawiki_images.tgz";
+	_sql_dump_dir="sql_dump";
+	_wiki_images_dir="wiki_images";
+
+	echo "#>> execute mysqldump for '$_conf_wikini__target_db' -> $_sql_dump_file";
+	if [ ! -d $_sql_dump_dir ]; then
+		mkdir -p $_sql_dump_dir;
+	fi
+	(cd $_sql_dump_dir;
+	mysqldump -p -u root $_conf_wikini__target_db > $_sql_dump_file;
+	split -a 5 -l 10 -d $_sql_dump_file _sql_; sync; sleep 2; rm $_sql_dump_file;
+	)
+
+	echo "#>> enpack the uploaded $_conf_wikini__target_db images";
+	if [ ! -d $_wiki_images_dir ]; then
+		mkdir -p $_wiki_images_dir;
+	fi
+	(cd $_conf_wikini__mediawiki_var_root; sudo tar cf - images) | gzip -c > ${_wiki_images_dir}/${_var_lib_mediawiki_images_tarball};
 	)
 #------------------------------------------------
 fi
+
+
+
+
