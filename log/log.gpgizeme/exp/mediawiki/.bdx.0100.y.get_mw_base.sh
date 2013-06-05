109f29217b196ced2fb85170c3b0b3e4a6e8cbce
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-06-05 19:33:22 +0900

    (re-install: ubuntu 12.04.2 LTS on jimi-hendrix)

--- log/log.gpgizeme/exp/mediawiki/.bdx.0100.y.get_mw_base.sh
+++ log/log.gpgizeme/exp/mediawiki/.bdx.0100.y.get_mw_base.sh
@@ -6,3 +6,7 @@ curl -0 http://dumps.wikimedia.org/mediawiki/1.21/mediawiki-1.21.0.tar.gz > mw-1
 #curl -0 http://dumps.wikimedia.org/mediawiki/1.20/mediawiki-1.20.6.tar.gz > mw-1.20.6.tgz;
 #curl -0 http://dumps.wikimedia.org/mediawiki/1.19/mediawiki-1.19.2.tar.gz > mw-1.19.2.tgz;
 #curl -0 http://dumps.wikimedia.org/mediawiki/1.15/mediawiki-1.15.5.tar.gz > mw-1.15.5.tgz;
+
+echo "
+PLEASE make sure that 'memcached' is running properly!!!
+"
