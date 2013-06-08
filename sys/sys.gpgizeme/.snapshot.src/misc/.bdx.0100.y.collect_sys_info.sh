ecbc2d9fc59c262039bf185437645e77d8456bf1
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-06-09 02:13:45 +0900

    bpk; .snapshot.src;

--- sys/sys.gpgizeme/.snapshot.src/misc/.bdx.0100.y.collect_sys_info.sh
+++ sys/sys.gpgizeme/.snapshot.src/misc/.bdx.0100.y.collect_sys_info.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 ## .bdx.0100.y.collect_sys_info.sh
-##tstamp: 20121006_211639
+##_ver=20121006_211639
+##_ver=20130608_223338
 
 
+#--------------------------------------------------------------------------------
+ls -alF ~/x/ > x_list.log
+#--------------------------------------------------------------------------------
 #sudo aa-status > aa-status.log		## 'apparmor' related stuff
 sudo ifconfig > ifconfig.log
 sudo iptables -S > iptables.log
@@ -16,9 +20,10 @@ sudo lsscsi > lsscsi.log
 sudo lsusb > lsusb.log 
 sudo mount > mount.log
 sudo route -n > route-n.log
+sudo cp -p /var/lib/mediawiki/LocalSettings.php .
+sudo cp -p /var/www/index.html .
+#--------------------------------------------------------------------------------
 
-
-ls -alF ~/x/ > x_list.log
 
 # _whoami=$(whoami);
 # sudo chown $_whoami:$_whoami *.log
