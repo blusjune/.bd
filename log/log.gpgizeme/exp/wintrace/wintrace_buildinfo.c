125aa40a3b5ec84a426511fa3448024273aa5dac
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-07-05 21:55:44 +0900

    bpk; wintrace; cgopt;

--- log/log.gpgizeme/exp/wintrace/wintrace_buildinfo.c
+++ log/log.gpgizeme/exp/wintrace/wintrace_buildinfo.c
@@ -1,1 +1,1 @@
-char * wintrace_buildinfo_20130630_013000 = "WinTrace (c) 2013 // Author: Brian M. JUNG // Build#: 20130630_013000";
+char * wintrace_buildinfo_20130701_090916 = "WinTrace (c) 2013 // Author: Brian M. JUNG // Build#: 20130701_090916";
