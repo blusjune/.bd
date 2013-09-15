37ae08ef52b313e5eb0d2f9823c4bc91f58569fd
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-09-15 15:34:18 +0900

    bpk; puzzle_challenge; wikini;

---
+++ log/log.gpgizeme/exp/puzzle_challenge/gchq--canyoufindit.co.uk/cmd.str_xform_01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os, sys
+
+
+'''
+for line in sys.stdin:
+	print "(" + line + ")"
+'''
+
+
+for line in sys.stdin:
+	clist = list(line)
+	for char in clist:
+		print char
+
+
+
+
